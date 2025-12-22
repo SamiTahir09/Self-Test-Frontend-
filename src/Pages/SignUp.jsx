e9c2e55f9de924946f32cb154407bcd7f8369706
SamiTahir09 <samitahir858@gmail.com>
2025-12-22 05:35:28 -0800

    nip

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -30,7 +30,7 @@ const SignUp = () => {
 
         try {
             const userData = await axios.post(
-                'https://self-test-backend-production-33f3.up.railway.app/api/user/save',
+                'https://self-test-backend-production.up.railway.app/api/user/save',
                 { name, email, password }
             );
 
